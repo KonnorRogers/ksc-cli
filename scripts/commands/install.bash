728917ff544c9251993a6c07381d8f0aaf2f003f
paramagicdev <konnor5456@gmail.com>
2019-08-20 05:21:14 -0400

    installing packages now works as intended

--- scripts/commands/install.bash
+++ scripts/commands/install.bash
@@ -7,40 +7,44 @@ install_all_packages(){
   install_npm_packages
   install_pip_packages
   install_snap_packages
-  install_zsh_plugins_packages
 }
 export -f install_all_packages
 
 install_apt_packages(){
   packages="${1:-$KSC_APT_ALL}"
+  sudo apt update
+  sudo apt upgrade -y
   sudo apt install -y $packages
 }
 export -f install_apt_packages
 
 install_gems_packages(){
   gems="${1:-$KSC_GEMS}"
-
+  gem install $gems
 }
 export -f install_gems_packages
 
 install_npm_packages(){
   npm_packages="${1:-$KSC_NPM_PACKAGES}"
-
+  npm install --global $npm_packages
 }
 export -f install_npm_packages
 
 install_pip2_packages(){
   pip2="${1:-$KSC_PIP2_PACKAGES}"
+  pip2 install --user --upgrade $pip2
 }
 export -f install_pip2_packages
 
 install_pip3_packages(){
-  pip2="${1:-$KSC_PIP3_PACKAGES}"
+  pip3="${1:-$KSC_PIP3_PACKAGES}"
+  pip3 install --user --upgrade $pip3
 }
 export -f install_pip3_packages
 
 install_snap_packages(){
   snap="${1:-$KSC_SNAP_PACKAGES}"
+  sudo snap install $snap
 }
 export -f install_snap_packages
 
