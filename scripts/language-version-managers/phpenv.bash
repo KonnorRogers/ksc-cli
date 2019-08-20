09cdfd08c167cfdb0efce5eaac33fb5441266470
paramagicdev <konnor5456@gmail.com>
2019-08-20 03:37:40 -0400

    Added installations and usages of languages

--- scripts/language-version-managers/phpenv.bash
+++ scripts/language-version-managers/phpenv.bash
@@ -4,9 +4,6 @@ set -e
 main(){
   echo "Installing PHPENV..."
   curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer \ | bash
-
-  phpenv install --skip-existing "$PHP_VERSION"
-  phpenv global "$PHP_VERSION"
 }
 
 main
